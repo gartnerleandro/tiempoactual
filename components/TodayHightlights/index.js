c4a1ac6b2f5d1228831dbc28591efca054158520
gartnerleandro <gartnerleandro@gmail.com>
2022-05-20 21:25:10 +0200

    feature: add desktop styles

--- components/TodayHightlights/index.js
+++ components/TodayHightlights/index.js
@@ -18,54 +18,56 @@ export default function TodayHightlights({
   return (
     <div className={styles.container}>
       <h4 className={styles.title}>Destacados de hoy</h4>
-      <div className={styles.card}>
-        <p className={styles.cardTitle}>Estado del viento</p>
-        <p className={styles.cardData}>
-          {getKmPerHours(windSpeed)}
-          <span className={styles.symbol}>km/h</span>
-        </p>
-        <p className={styles.windDirection}>
-          <i style={{ transform: `rotate(${windDeg}deg)` }}>
-            <Direction />
-          </i>
-          {getWindDirection(windDeg)}
-        </p>
-      </div>
-      <div className={styles.card}>
-        <p className={styles.cardTitle}>Humedad</p>
-        <p className={styles.cardData}>
-          {humidity}
-          <span className={styles.symbol}>%</span>
-        </p>
-        {
-          <div className={styles.sliderWrapper}>
-            <div className={styles.sliderLabels}>
-              <span>0</span>
-              <span>50</span>
-              <span>100</span>
+      <div className={styles.cardsWrapper}>
+        <div className={styles.card}>
+          <p className={styles.cardTitle}>Estado del viento</p>
+          <p className={styles.cardData}>
+            {getKmPerHours(windSpeed)}
+            <span className={styles.symbol}>km/h</span>
+          </p>
+          <p className={styles.windDirection}>
+            <i style={{ transform: `rotate(${windDeg}deg)` }}>
+              <Direction />
+            </i>
+            {getWindDirection(windDeg)}
+          </p>
+        </div>
+        <div className={styles.card}>
+          <p className={styles.cardTitle}>Humedad</p>
+          <p className={styles.cardData}>
+            {humidity}
+            <span className={styles.symbol}>%</span>
+          </p>
+          {
+            <div className={styles.sliderWrapper}>
+              <div className={styles.sliderLabels}>
+                <span>0</span>
+                <span>50</span>
+                <span>100</span>
+              </div>
+              <div className={styles.sliderContent}>
+                <span
+                  className={styles.sliderLine}
+                  style={{ width: `${humidity}%` }}
+                />
+              </div>
             </div>
-            <div className={styles.sliderContent}>
-              <span
-                className={styles.sliderLine}
-                style={{ width: `${humidity}%` }}
-              />
-            </div>
-          </div>
-        }
-      </div>
-      <div className={styles.card}>
-        <p className={styles.cardTitle}>Visibilidad</p>
-        <p className={styles.cardData}>
-          {getKmFromMts(visibility)}
-          <span className={styles.symbol}>km</span>
-        </p>
-      </div>
-      <div className={styles.card}>
-        <p className={styles.cardTitle}>Presión del aire</p>
-        <p className={styles.cardData}>
-          {pressure}
-          <span className={styles.symbol}>hPa</span>
-        </p>
+          }
+        </div>
+        <div className={styles.card}>
+          <p className={styles.cardTitle}>Visibilidad</p>
+          <p className={styles.cardData}>
+            {getKmFromMts(visibility)}
+            <span className={styles.symbol}>km</span>
+          </p>
+        </div>
+        <div className={styles.card}>
+          <p className={styles.cardTitle}>Presión del aire</p>
+          <p className={styles.cardData}>
+            {pressure}
+            <span className={styles.symbol}>hPa</span>
+          </p>
+        </div>
       </div>
     </div>
   );
